3658b150092ee33d3332278c8d9be2eeaa46237e
Joao Dantas <jcfdantas@gmail.com>
2020-11-15 21:57:59 -0300

    Retirando proxy

--- src/app/core/shared/api-config.ts
+++ src/app/core/shared/api-config.ts
@@ -1,5 +1,5 @@
 export const apiConfig = {
-    get_pokemon: '/api/pokemon/',
-    get_species: '/api/pokemon-species/',
-    get_types: '/api/type/'
+    get_pokemon: 'https://pokeapi.co/api/v2/pokemon/',
+    get_species: 'https://pokeapi.co/api/v2/pokemon-species/',
+    get_types: 'https://pokeapi.co/api/v2/type/'
 }
